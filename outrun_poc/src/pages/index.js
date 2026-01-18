289d71ea5ca8ebb273d530805d11f97d25aaa7e8
OUTRUN <run@outrun.co.za>
2026-01-18 17:31:40 +0200

    Add files via upload

--- outrun_poc/src/pages/index.js
+++ outrun_poc/src/pages/index.js
@@ -4,25 +4,41 @@
 import { Container, Stack, Typography } from "@mui/material";
 import StravaConnectButton from "../components/auth/StravaConnectButton";
 import Image from "next/image";
-import logo from "../assets/name.png";
+import AppHeader from "../components/common/AppHeader";
+import name from "../assets/name.png";
+import logo from "../assets/logo.png";
 
 export default function LandingPage() {
   return (
-    <Container maxWidth="xs">
-      <Stack spacing={4} mt={10} alignItems="center">
-        <Image
-          src={logo}
-          alt="OUTRUN"
-          width={220}
-          priority
-        />
+    <>
+      <AppHeader />
 
-        <Typography variant="body2" align="center">
-          Premium Virtual Running Challenge
-        </Typography>
+      <Container maxWidth="xs">
+        <Stack spacing={4} mt={10} alignItems="center">
+          <Image
+            src={name}
+            alt="OUTRUN_name"
+            width={220}
+            priority
+          />
+          <Image
+            src={logo}
+            alt="OUTRUN_logo"
+            width={220}
+            priority
+          />
 
-        <StravaConnectButton />
-      </Stack>
-    </Container>
+          <Typography variant="body2" align="center">
+            Premium Virtual Running Challenge
+          </Typography>
+
+          // Add button to Join Challenge (nav to ticket sales) - need to think how to use this to ensure only auth participants can link to Strava
+          // Add countdown to challenge start
+          // Add button to rules 
+
+          <StravaConnectButton />
+        </Stack>
+      </Container>
+    </>
   );
 }
